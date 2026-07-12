937ea16048a9cbde3c13c9585897edf7eb1ed025
Chetan Gorantla <65378979+ChetanGorantla@users.noreply.github.com>
2026-07-12 14:22:53 -0400

    Add: lowest-common-ancestor-of-a-binary-tree-iii - submission-0

---
+++ Data Structures & Algorithms/lowest-common-ancestor-of-a-binary-tree-iii/submission-0.py
@@ -0,0 +1,29 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+        self.parent = None
+"""
+
+class Solution:
+    def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
+        # just traverse the parents and keep track of visited? see the first crossover?
+        visited = set()
+
+        while p:
+            visited.add(p)
+            p = p.parent
+        while q:
+            if q in visited:
+                return q
+            visited.add(q)
+            q = q.parent
+        return None
+    
+    
+        
+
+        
